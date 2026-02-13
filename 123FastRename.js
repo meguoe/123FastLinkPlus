5810ac7247ae0d9a935229d1f63558c4d288606a
meguoe <meguoe@163.com>
2026-02-13 11:03:14 +0800

    docs: 更新脚本版本号至1.0.1

--- 123FastRename.js
+++ 123FastRename.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         123FastRename
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @description  123云盘文件批量重命名助手
 // @author       meguoe
 // @license      Apache-2.0
